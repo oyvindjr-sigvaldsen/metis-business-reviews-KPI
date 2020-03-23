6f773c490c0ad5616e5771f8d061ef1be475f33a
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-23 19:30:41 +0100

    Created modules dir for storing retrieve_html_file_paths.rb

---
+++ yelp/modules/retrieve_html_file_paths.rb
@@ -0,0 +1,22 @@
+def retrieve_html_file_paths(json_file_path)
+
+	begin
+		business_urls = File.open json_file_path
+		json_data = JSON.load business_urls
+		business_urls.close
+
+		main_array = json_data["business_urls"]
+		html_files = []
+
+		(0..main_array.length-1).step(1) do |i|
+			business_name = main_array[i]["business_name"]
+			html_files.push("business-html-files/#{business_name}.html")
+		end
+
+		return html_files
+
+	# catch incorrect json_file_path error
+	rescue nil::NilClass => error
+		puts error
+	end
+end
