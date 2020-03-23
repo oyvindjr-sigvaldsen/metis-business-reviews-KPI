95b001f6dcb3a6075223e6d91331b4db857676d2
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-23 18:55:54 +0100

    Removed unnecessary spaces

--- yelp/classes/business_info.rb
+++ yelp/classes/business_info.rb
@@ -1,10 +1,10 @@
 class BusinessInfo
 
 	def initialize(name, number_of_reviews, price_point, cuisines, star_rating)
-		@name = name.to_s
+		@name = name.to_s.tr("'", "")
 		@number_of_reviews = number_of_reviews.chomp("reviews").to_i
 		@price_point = price_point.to_s.chomp(" ")
-		@cuisines = cuisines.to_s.split(",").to_s
+		@cuisines = cuisines.to_s.tr(" ", "").split(",").to_a
 		@star_rating = star_rating.to_s.chomp("star rating")
 	end
 
