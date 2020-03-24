3a5464025008bcea5fbe502246a3b7abf7047dae
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-24 14:31:56 +0100

    Finalised tripadvisor branch business_info.rb

--- tripadvisor/business_info.rb
+++ tripadvisor/business_info.rb
@@ -1,36 +1,38 @@
 require_relative "../requirements"
-require_relative "../global-modules/retrieve_sql_credentials"
-
 require_relative "../global-classes/business_info"
+
+require_relative "../global-modules/retrieve_sql_credentials"
+require_relative "../global-modules/push_business_info_sql"
 require_relative "../global-modules/retrieve_html_file_paths"
 
 def business_info_scraper(html_files)
 
-	# TODO: create class for business, review_user
 	begin
 		business_info_instances = []
 
 		html_files.each do |html_file|
 
-			# TODO: make use open-uri or net/http to open live webpage using URL
 			parsed_page = Nokogiri::HTML(open(html_file))
 
-			begin
+			name = parsed_page.css("h1").text[/[^,]+/].tr("''", "")
+			number_of_reviews = parsed_page.css("div.ratingContainer").css("a").text.chomp("reviews").to_i
+			price_point = parsed_page.xpath("//*[@id='taplc_resp_rr_top_info_rr_resp_0']/div/div[3]/div[3]/div/a[1]").text
+			star_rating = parsed_page.css("span.restaurants-detail-overview-cards-RatingsOverviewCard__overallRating--nohTl").text.to_f
 
-				name = parsed_page.css("h1").text
-				num_reviews = parsed_page.css("a.restaurants-detail-overview-cards-RatingsOverviewCard__ratingCount--DFxkG").text
-				price_point = parsed_page.xpath("//*[@id='taplc_resp_rr_top_info_rr_resp_0']/div/div[3]/div[3]/div/a[1]").text
+			cuisines = []
+			raw_cuisines = parsed_page.css("div.header_links").css("a").to_a.drop(1)
 
-				cuisines = []
-				raw_cuisines = parsed_page.css("div.header_links").css("a").to_a.drop(1)
-				puts raw_cuisines.length
-
-				#business_info_instance = BusinessInfo.new(name, number_of_reviews, price_point, )
-				#business_info_instances.push(business_info_instance)
-
-			rescue
-				next
+			(0..raw_cuisines.length).step(1) do |i|
+				begin
+					cuisine = raw_cuisines[i].text.to_s
+					cuisines.push(cuisine)
+				rescue
+					next
+				end
 			end
+
+			business_info_instance = BusinessInfo.new(name, number_of_reviews, price_point, cuisines, star_rating)
+			business_info_instances.push(business_info_instance)
 		end
 		return business_info_instances
 
@@ -40,35 +42,6 @@ def business_info_scraper(html_files)
 	end
 end
 
-def push_business_info_sql(business_info_instances)
-
-	# https://docs.microsoft.com/en-us/azure/mysql/connect-ruby
-	username, password = retrieve_sql_credentials("../mysql_credentials.json")
-	connection = Mysql2::Client.new(:host => "localhost", :username => username, :database => "metis_development", :password => password)
-
-	connection.query("DELETE FROM business_info")
-
-	business_info_instances.each do |instance|
-
-		begin
-			connection = Mysql2::Client.new(:host => "localhost", :username => username, :database => "metis_development", :password => password)
-			connection.query("INSERT INTO business_info VALUES(
-																'#{instance.instance_variable_get(:@name)}',
-																'#{instance.instance_variable_get(:@number_of_reviews)}',
-																'#{instance.instance_variable_get(:@price_point)}',
-																'#{instance.instance_variable_get(:@cuisines)}',
-																'#{instance.instance_variable_get(:@star_rating)}'
-															)")
-			connection.close
-
-		# catch Mysql2 syntax and other errors
-		rescue Mysql2::Error => error
-			puts error
-		end
-	end
-end
-
 html_files = retrieve_html_file_paths("retrieve-html-files/data-files/business_urls.json")
-puts html_files
 business_info_instances = business_info_scraper(html_files)
-#push_business_info_sql(business_info_instances)
+push_business_info_sql(business_info_instances, "tripadvisor_business_info")
