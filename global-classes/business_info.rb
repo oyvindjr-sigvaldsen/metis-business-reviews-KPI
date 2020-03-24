44151357a52bd2ecd1d8a9a8523ba6ba3c65f003
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-24 14:33:09 +0100

    Temporary changes to business_info-rb

--- global-classes/business_info.rb
+++ global-classes/business_info.rb
@@ -1,11 +1,17 @@
 class BusinessInfo
 
 	def initialize(name, number_of_reviews, price_point, cuisines, star_rating)
-		@name = name.to_s.tr("'", "")
-		@number_of_reviews = number_of_reviews.chomp("reviews").to_i
-		@price_point = price_point.to_s.chomp(" ")
-		@cuisines = cuisines.to_s.tr(" ", "").split(",").to_a
-		@star_rating = star_rating.to_s.chomp("star rating")
+		#@name = name.to_s.tr("'", "")
+		#@number_of_reviews = number_of_reviews.chomp("reviews").to_i
+		#@price_point = price_point.to_s.chomp(" ")
+		#@cuisines = cuisines.to_s.tr(" ", "").split(",").to_a
+		#@star_rating = star_rating.to_s.chomp("star rating")
+
+		@name = name
+		@number_of_reviews = number_of_reviews
+		@price_point = price_point
+		@cuisines = cuisines
+		@star_rating = star_rating
 	end
 
 	def display
