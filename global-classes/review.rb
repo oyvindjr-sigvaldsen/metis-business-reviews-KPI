8c460720658e5c318b2d39df66acb07bf00164a4
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-24 17:59:10 +0100

    Finished review.rb class file

--- global-classes/review.rb
+++ global-classes/review.rb
@@ -1,14 +1,17 @@
 class Review
 
-	def initialize(star_rating, date, check_in, photos, text)
+	def initialize(title, visit_date, review_date, star_rating, text, photos, username, number_of_reviews)
+		@title = title
+		@visit_date = visit_date
+		@review_date = review_date
 		@star_rating = star_rating
-		@date = date
-		@check_in = check_in
-		@photos = photos
 		@text = text
+		@photos = photos
+		@username = username
+		@number_of_reviews = number_of_reviews
 	end
 
 	def display
-		puts "\n**#{@star_rating}**\n#{@date}\n#{@check_in}\n#{@photos}\n#{@text}\n"
+		puts "\n**#{@title}**\n#{@visit_date}\n#{@review_date}\n#{@star_rating}\n#{@text}\n#{@photos}\n#{@username}\n#{@number_of_reviews}"
 	end
 end
