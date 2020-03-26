4c9333ec7981d131bc8de50ec9b71546adb29af7
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-26 18:26:01 +0100

    Pushing reviews to single db table

---
+++ global-modules/push_reviews_sql.rb
@@ -0,0 +1,30 @@
+def push_reviews_sql(review_instances, sql_table)
+
+	# https://docs.microsoft.com/en-us/azure/mysql/connect-ruby
+	username, password = retrieve_sql_credentials("../mysql_credentials.json")
+	connection = Mysql2::Client.new(:host => "localhost", :username => username, :database => "metis_development", :password => password)
+
+	connection.query("DELETE FROM #{sql_table}")
+
+	review_instances.each do |instance|
+
+		begin
+			connection = Mysql2::Client.new(:host => "localhost", :username => username, :database => "metis_development", :password => password)
+			connection.query("INSERT INTO #{sql_table} VALUES(
+																'#{instance.instance_variable_get(:@title)}',
+																'#{instance.instance_variable_get(:@visit_date)}',
+																'#{instance.instance_variable_get(:@review_date)}',
+																'#{instance.instance_variable_get(:@star_rating)}',
+																'#{instance.instance_variable_get(:@text)}',
+																'#{instance.instance_variable_get(:@photos)}',
+																'#{instance.instance_variable_get(:@username)}',
+																'#{instance.instance_variable_get(:@number_of_reviews)}'
+															)")
+			connection.close
+
+		# catch Mysql2 syntax and other errors
+		rescue Mysql2::Error => error
+			puts error
+		end
+	end
+end
--- tripadvisor/modules/tripadvisor_reviews.rb
+++ tripadvisor/modules/tripadvisor_reviews.rb
@@ -2,34 +2,34 @@ require_relative "../../requirements"
 require_relative "../../global-classes/review"
 
 require_relative "../../global-modules/retrieve_sql_credentials"
+require_relative "../../global-modules/push_reviews_sql"
 require_relative "../../global-modules/retrieve_html_file_paths"
 
 def reviews_scraper(html_files)
 
 	begin
+		review_instances = []
 
 		html_files.each_with_index do |html_file, i|
 
 			parsed_page = Nokogiri::HTML(open(html_file))
 
-			# TODO: make get hidden html text using witer
+
 			# get url for current html_file
 			business_urls = File.open "retrieve-html-files/data-files/business_urls.json"
 			json_data = JSON.load business_urls
 			business_urls.close
 
 			html_file_url = json_data["business_urls"][i]["url"]
-			#browser = Watir::Browser.new :safari
-			#browser.goto(html_file_url)
+			browser = Watir::Browser.new :safari
+			browser.goto(html_file_url)
 
 			review_wrapper = parsed_page.css("div.ui_column")
-			titles = []
 
 			# for clicking "more" buttons and revealing full review text
-			#more_onclick_span =  browser.spans(visible: true, class: ["taLnk", "ulBlueLinks"])
-			#more_onclick_span[0].fire_event :click
-
-			#sleep 2
+			more_onclick_span =  browser.spans(visible: true, class: ["taLnk", "ulBlueLinks"])
+			more_onclick_span[0].fire_event :click
+			sleep 2
 
 			# 0.. num of reviews per page = 10
 			(0..review_wrapper.css("span.noQuotes").length-1).step(1) do |i|
@@ -62,33 +62,37 @@ def reviews_scraper(html_files)
 					star_rating = raw_star_rating.attribute("class").text.scan(/\d+/).first.to_f / 10.00
 
 					# text
-					#text_raw = browser.ps(visible: true, class: "partial_entry")[i].inner_text
+					text_raw = browser.ps(visible: true, class: "partial_entry")[i].inner_text
 
 					# 	>> capitalize every letter following full stops and other sentence enders
-					#text = text_raw.split.join(" ").gsub(/([a-z])((?:[^.?!]|\.(?=[a-z]))*)/i) { $1.upcase + $2.rstrip }
+					text = text_raw.split.join(" ").gsub(/([a-z])((?:[^.?!]|\.(?=[a-z]))*)/i) { $1.upcase + $2.rstrip }.tr("''", "")
 
 					# photos
-					begin
+					photos_raw = parsed_page.css("div.review-container")[i].css("div.inlinePhotosWrapper").css("img.centeredImg.noscript")
 
-						puts title
-						photos_raw = review_wrapper.css("div.inlinePhotosWrapper")[0].css("img.centeredImg").length
-						pp photos_raw
+					photos = []
 
-						if photos_raw.include? ".jpg"
-							pp photos_raw
-						else
-							nil
-							pp photos_raw
+					if photos_raw.length > 0
+
+						(0..photos_raw.length-1).step(1) do |x|
+							photo_src = photos_raw[x].attribute("src").to_s
+							photos.push(photo_src)
 						end
-
-					rescue
-						"NO IMAGE"
-						next
+					else
+						nil
 					end
 
-					#puts text
-					puts "_________________\n"
-					puts "_________________\n"
+					# username
+					username_raw = parsed_page.css("div.member_info")[i]
+
+					username = username_raw.css("div")[0].text.to_s.split.join(" ")
+
+					# number_of_reviews
+					number_of_reviews_raw = parsed_page.css("span.badgeText")
+					number_of_reviews = number_of_reviews_raw[i].text.chomp("reviews").split.join(" ").to_i
+
+					review_instance = Review.new(title, visit_date.to_s, review_date.to_s, star_rating, text, photos, username, number_of_reviews)
+					review_instances.push(review_instance)
 
 				# in case of nil value error
 				rescue NoMethodError => error
@@ -97,9 +101,9 @@ def reviews_scraper(html_files)
 					next
 				end
 			end
-			#browser.quit
-			puts "\n\n"
+			browser.quit
 		end
+		return review_instances
 
 	# catch 'no such file'
 	rescue Errno::ENOENT => error
@@ -108,4 +112,5 @@ def reviews_scraper(html_files)
 end
 
 html_files = retrieve_html_file_paths("retrieve-html-files/data-files/business_urls.json")
-reviews_scraper(html_files)
+review_instances = reviews_scraper(html_files)
+push_reviews_sql(review_instances, "tripadvisor_reviews")
