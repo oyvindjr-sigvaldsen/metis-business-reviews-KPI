ba20acc9d2f988414abc9b140e3841c76da6f0d3
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-27 19:44:33 +0100

    Created dedicated folder for global sql modules

---
+++ global-modules/sql/retrieve_sql_credentials.rb
@@ -0,0 +1,8 @@
+def retrieve_sql_credentials(credentials_path)
+
+	credentials_file = open(credentials_path)
+	json = credentials_file.read
+	parsed_json = JSON.parse(json)
+
+	return parsed_json["username"], parsed_json["password"]
+end
